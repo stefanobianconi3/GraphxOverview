d23686bc7ac02cf7f575c6a7d4e8bcb6ba6174b0
Stefano <Bianconi>
2020-03-15 18:38:15 +0100

    Graph created
    basic examples added

--- src/main/scala/Analysis.scala
+++ src/main/scala/Analysis.scala
@@ -1,4 +1,7 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark._
+import org.apache.spark.graphx._
+import org.apache.spark.rdd.RDD
 
 object Analysis {
   def main(args: Array[String]) {
@@ -21,14 +24,15 @@ object Analysis {
     val dfVertices = jsonFile.select(explode(jsonFile("vertices"))).toDF("vertices")
     
     //extract all the column of interest
-    val dfetV = dfVertices.select("vertices._id", "vertices._label", "vertices.uri", "vertices._ctx", "vertices.category", "vertices.type")
-    
+    val dfetV = dfVertices.select("vertices._id", "vertices.menuLabel", "vertices.uri", "vertices._ctx", "vertices.category", "vertices.type")
+    //val dfetV = dfVertices.select("vertices._id", "vertices.menuLabel", "vertices.type", "vertices.uri")
+
     //create temporary view to be queried
     dfetV.createOrReplaceTempView("queryetV")
     //dfetV.count()
     
     //persist in memory the RDD
-    val etlV = spark.sql("SELECT  * FROM queryetV WHERE _id BETWEEN 0 AND 61500").persist()
+    val etlV = spark.sql("SELECT  _id, menuLabel, type, uri  FROM queryetV WHERE _id BETWEEN 0 AND 61500").persist()
     //etlV.show()
 
     //select and explode all the fields in "edges" 
@@ -36,14 +40,36 @@ object Analysis {
 
     //extract all the column of interest
     val dfetE = dfEdges.select("edges._id", "edges._label", "edges.uri", "edges._ctx", "edges._outV", "edges._inV")
+   
     //create temporary view
     dfetE.createOrReplaceTempView("queryetE")
     //persist in memory
-    val etlE = spark.sql("SELECT  * FROM queryetE WHERE _id BETWEEN 593 AND 95950").persist()
+    val etlE = spark.sql("SELECT  _inV, _outV, _label FROM queryetE WHERE _id BETWEEN 593 AND 95950").persist()
     //etlE.show()
 
-    //////GRAPHX
-    val sqlcustom4 = spark.sql("SELECT DISTINCT type FROM queryetl WHERE _id BETWEEN 0 AND 61500").show()
+    //Show me the 
+    val vertexTypes = spark.sql("SELECT DISTINCT type FROM queryetV WHERE _id BETWEEN 0 AND 61500").show()
+
+    //remove the rows with same id
+    etlV.dropDuplicates("_id")
+
+    //create variable to be inserted later inside the grap
+    val verticesG: RDD[(VertexId, (String, String, String))] = etlV.rdd.map(row => (row.getAs[Long]("_id"), (row.getAs[String]("menuLabel"), row.getAs[String]("type"), row.getAs[String]("type"))))
+
+    val edgesE: RDD[Edge[String]] = etlE.rdd.map(row => Edge(row.getAs[Long]("_inV"), row.getAs[Long]("_outV"), row.getAs[String]("_label")))
+
+    val graph = Graph(verticesG, edgesE)
+
+    //graph.vertices.filter{ case (id, (a, b, c)) => b == "LuxMeter" }.count
+
+    //create triplets from the menuLabel param.
+    val facts: RDD[String] = graph.triplets.map(triplet => triplet.srcAttr._1 + " is " + triplet.attr + " of " + triplet.dstAttr._1)
+    //print all the triplets
+    facts.saveAsTextFile("/home/muletto/Desktop/dir")
+    
+
+
+
 
 
 
